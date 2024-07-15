d9f117f8c05360114fe5714b5c1e490957ea34dd
hironmoy-ray <hpray35@gmail.com>
2024-07-16 01:13:54 +0600

    Comparison

---
+++ Day-1/Comparison.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log( null > 0); // false because null is treated as Zero(0) in Comparison
+console.log( null == 0); // false because equality
+console.log( null >= 0); // true because null is treated as Zero(0) in Comparison
+
+
+console.log( undefined > 0); // always false for undefined
+console.log( undefined == 0);
+console.log( undefined >= 0);
+
+console.log("Abc" < "ABc"); //  Letter Order Fact & also Case Sensitive Fact.
+console.log("Hiron" < "hiron");
