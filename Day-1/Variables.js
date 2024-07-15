5007d0b532bf6d6a1596e130ca06d085b56d50ef
hironmoy-ray <hpray35@gmail.com>
2024-07-15 18:13:35 +0600

    let var const

---
+++ Day-1/Variables.js
@@ -0,0 +1,32 @@
+/* ----------------------------------------------------
+Discussing About "let" Variables 
+------------------------------------------------------*/
+let myName; // Variable Declare & Assigning value is undefine
+console.log(myName); // undefine datatype 
+console.log(typeof myName);
+
+console.log(typeof (typeof myName));
+console.table([myName, (typeof myName)])
+
+myName = "Hironmoy"; 
+myName= "Ray"; // Reassign but Can't ReDeclare.
+
+
+/* ----------------------------------------------------
+Discussing About "var" Variables 
+------------------------------------------------------*/
+var myText; // Declare & Assigning value is undefine.
+var myText = "Hello Var"; // ReDeclare.
+myText = " Welcome Var" // ReAssign.
+
+console.log(myText);
+console.log(typeof myText);
+
+
+/* ----------------------------------------------------
+Discussing About "const" Variable 
+------------------------------------------------------*/
+const PI = 3.1416; // Declare & Assigning all together
+console.log(PI)
+console.log(typeof PI)
+
