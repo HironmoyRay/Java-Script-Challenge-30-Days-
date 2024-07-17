cbff844d5ac9a8daa6c8312abaa59526d18eaba2
hironmoy-ray <hpray35@gmail.com>
2024-07-17 13:34:12 +0600

    Stack vs Heap

---
+++ Day-1/Memory.js
@@ -0,0 +1,19 @@
+// Stack / Primitive Data Memory Allocation 
+let myName = "Hironmoy";
+console.log(myName);
+
+let nickName = myName; // Copy 
+nickName = "Ray";
+console.log(myName); 
+console.log(nickName);
+
+// Heap Memory / Non Primitive Memory Allocation
+let userOne = 
+{
+    id:1705045,
+    name: "Hironmoy"
+}
+let userTwo = userOne; // Reference
+
+userTwo.name = "Ray"
+console.log(userOne.name);
