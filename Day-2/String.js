7599dc8e25f914cdaae91129b1bc421c3ee83ef5
hironmoy-ray <hpray35@gmail.com>
2024-07-17 18:26:45 +0600

    string methods

--- Day-2/String.js
+++ Day-2/String.js
@@ -1,5 +1,27 @@
+/* substring method */
+const sports = "Cricket"
+const subSports = sports.substring(0,2); // Negative Value not allowed. Neg Value treated as Zero (0)
+console.log(subSports);
+
+
+/* slice method */
 const myName = "Hironmy"
 console.log(myName.length);
-
-const sliceName = myName.slice(-7,4);
+const sliceName = myName.slice(-1,4); // Neg Value Allowed
+// for negVal => start = ( negVal + length );
+// after that start less than Zero , then stat = 0;
+// or start greater than length , then empty array returned.
 console.log(sliceName);
+
+/* trim method */
+let game = "     PUBG - Free Fire      "
+console.log(game.trim()); // Remove white space from start & end not middle
+
+/* replace Method */
+let url = "https://hironmoy_ray/hp%20ray"
+console.log(url.replace('%20', "-"));
+
+/* Split Method */
+let fruits = 'apple mango banana'
+console.log(fruits.split(' '));
+// console.log(fruits.split('a'));
